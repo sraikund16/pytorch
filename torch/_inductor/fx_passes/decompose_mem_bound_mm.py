a1e2b4ce80ab1a5529774b686e675b975a8435eb
Menglu Yu <mengluy@meta.com>
2024-03-14 17:40:20 -0700

    [PT2][Inductor] Customize Split Cat (#121915)
    Summary:
    
    Currently, we only enabled the group batch fusion customization, we also enable the split cat customization.
    
    Test Plan:
    ```
    buck2 run mode/opt //scripts/jackiexu0313/pt2:local_model_with_pt2 -- --test_mode batch-split --model_type "cmf" --flow_id 524546542
    ```
    P1196013839
    
    Differential Revision: D54861682

--- torch/_inductor/fx_passes/decompose_mem_bound_mm.py
+++ torch/_inductor/fx_passes/decompose_mem_bound_mm.py
@@ -6,15 +6,8 @@ from torch import Tensor
 from torch._dynamo.utils import counters
 from torch._inductor import utils
 
-from ..pattern_matcher import (
-    Arg,
-    CallFunction,
-    config_flag,
-    Ignored,
-    Match,
-    register_graph_pattern,
-)
-from .post_grad import decompose_mm_pass
+from ..pattern_matcher import Arg, CallFunction, Ignored, Match, register_graph_pattern
+from .split_cat import get_config_flag, get_pattern_matcher_pass
 
 aten = torch.ops.aten
 log = logging.getLogger(__name__)
@@ -125,8 +118,10 @@ def print_decompose_pattern(match: Match, inputs: List[torch.fx.Node]):
 
 @register_graph_pattern(
     CallFunction(aten.bmm, Arg(), Arg()),
-    pass_dict=decompose_mm_pass,
-    extra_check=config_flag("decompose_mem_bound_mm"),
+    pass_dict=get_pattern_matcher_pass("decompose_mem_bound_mm"),
+    extra_check=get_config_flag("decompose_mem_bound_mm"),
+    pre_grad=False,
+    optimus=True,
 )
 def decompose_bmm(match: Match, mat1: torch.fx.Node, mat2: torch.fx.Node):
     def repl(mat1, mat2):
@@ -141,8 +136,10 @@ def decompose_bmm(match: Match, mat1: torch.fx.Node, mat2: torch.fx.Node):
 
 @register_graph_pattern(
     CallFunction(aten.addmm, Arg(), Arg(), Arg()),
-    pass_dict=decompose_mm_pass,
-    extra_check=config_flag("decompose_mem_bound_mm"),
+    pass_dict=get_pattern_matcher_pass("decompose_mem_bound_mm"),
+    extra_check=get_config_flag("decompose_mem_bound_mm"),
+    pre_grad=False,
+    optimus=True,
 )
 def decompose_addmm(
     match: Match,
@@ -162,8 +159,10 @@ def decompose_addmm(
 
 @register_graph_pattern(
     CallFunction(aten.mm, CallFunction(aten.permute, Arg(), Ignored()), Arg()),
-    pass_dict=decompose_mm_pass,
-    extra_check=config_flag("decompose_mem_bound_mm"),
+    pass_dict=get_pattern_matcher_pass("decompose_mem_bound_mm"),
+    extra_check=get_config_flag("decompose_mem_bound_mm"),
+    pre_grad=False,
+    optimus=True,
 )
 def decompose_mmt(
     match: Match,
@@ -182,8 +181,10 @@ def decompose_mmt(
 
 @register_graph_pattern(
     CallFunction(aten.mm, Arg(), Arg()),
-    pass_dict=decompose_mm_pass,
-    extra_check=config_flag("decompose_mem_bound_mm"),
+    pass_dict=get_pattern_matcher_pass("decompose_mem_bound_mm"),
+    extra_check=get_config_flag("decompose_mem_bound_mm"),
+    pre_grad=False,
+    optimus=True,
 )
 def decompose_mm(
     match: Match,
@@ -202,8 +203,10 @@ def decompose_mm(
 
 @register_graph_pattern(
     CallFunction(aten.mm, Arg(), Arg()),
-    pass_dict=decompose_mm_pass,
-    extra_check=config_flag("decompose_mem_bound_mm"),
+    pass_dict=get_pattern_matcher_pass("decompose_mem_bound_mm"),
+    extra_check=get_config_flag("decompose_mem_bound_mm"),
+    pre_grad=False,
+    optimus=True,
 )
 def decompose_mm_large_k(
     match: Match,
